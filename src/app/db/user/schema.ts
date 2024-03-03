1e405a44e85b1839c6425c0c479957cd08c96a64
Jordan Janakievski <jajanak10@gmail.com>
2024-03-03 14:18:35 -0500

    Update user schema to have name

--- src/app/db/user/schema.ts
+++ src/app/db/user/schema.ts
@@ -2,6 +2,7 @@ import { integer, text, pgTable } from "drizzle-orm/pg-core";
 
 export const todo = pgTable("users", {
   id: integer("id").primaryKey(),
+  name: text("name").notNull(),
   email: text("email").notNull(),
   password: text("password").notNull(),
 });
